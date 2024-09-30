3f78d5f14a848bd3b435537b29849f970d5f8e18
Rahul Pant <rahulpant2002@gmail.com>
2024-10-01 04:53:16 +0530

    Sanitisation in SignUP and Validation in Update

--- src/app.js
+++ src/app.js
@@ -5,6 +5,54 @@ const User = require('./models/user');
 const app = express();
 app.use(express.json());
 
+//Push the data of a User
+app.post('/signup', async(req, res)=>{
+    const userData = req.body;
+    const user = new User(userData);
+    try{
+        await user.save();
+        res.send('User added Successfully...');
+    }
+    catch(err) {
+        res.status(400).send('Error in Adding User!!! ' + err.message);
+    }
+});
+
+//Delete a User
+app.delete('/user', async(req, res)=>{
+    const userId = req.body.id;
+    try{
+        const user = await User.findByIdAndDelete({_id:userId});
+        if(!user) res.send("No User Exist with this ID...");
+        else res.send("User deleted Successfully...");
+    }
+    catch(err){
+        res.status(404).send("Something went wrong!!!");
+    }
+})
+
+//Update the Info of a User
+app.patch('/user/:id', async(req, res)=>{
+    const userId = req.params?.id;
+    const userData = req.body;
+    try{
+        const allowedUpdates = ["password", "age", "photoUrl", "about", "skills"];
+        const isAllowed = Object.keys(userData).every((k)=>allowedUpdates.includes(k));
+        if(!isAllowed) throw new Error("Updation not Allowed!!!");
+
+        const user = await User.findByIdAndUpdate(userId, userData,
+            {
+                returnDocument : "after",
+                runValidators : true
+            }
+        );
+        res.send("Data updated successfully...");
+    }
+    catch(err){
+        res.status(404).send("Couldn't Update!!! " + err.message);
+    }
+})
+
 //Get a User by emailId
 app.get('/user', async(req, res)=>{
     const userEmail = req.body.emailId;
@@ -22,8 +70,8 @@ app.get('/user', async(req, res)=>{
 
 //Get all the Users
 app.get('/feed', async(req, res)=>{
-    const allUsers = await User.find({});
     try{
+        const allUsers = await User.find({});
         if(!allUsers) res.send("No User Exists..");
         else res.send(allUsers);
     }
@@ -32,18 +80,6 @@ app.get('/feed', async(req, res)=>{
     }
 })
 
-//Push the data of a User
-app.post('/signup', async(req, res)=>{
-    const userData = req.body;
-    const user = new User(userData);
-    try{
-        await user.save();
-        res.send('User added Successfully...');
-    }
-    catch(err) {
-        res.status(400).send('Error in Adding User!!!');
-    }
-});
 
 connectDB()
     .then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,12 +1,64 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    firstName : String,
-    lastName : String,
-    emailId : String,
-    password : String,
-    age : Number,
-    gender : String,
+    firstName : {
+        type : String,
+        maxLength : 30,
+        required : true,
+        validate(value){
+            if(value.length < 4 || value.length > 30) throw new Error("Firstname is invalid!!!")
+        }
+    },
+    lastName : {
+        type : String,
+    },
+    emailId : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        trim : true,  
+    },
+    password : {
+        type : String,
+        required : true,
+        minLength : 6,
+        validate(value){
+            if(value.length < 6) throw new Error("Password length should atleast 6")
+        }
+    },
+    age : {
+        type : String,
+        min : 18,
+        validate(x){
+            if(x < 18) throw new Error("User must be 18+");
+        }
+    },
+    gender : {
+        type : String,
+        required : true,
+        validate(value){
+            if(!["male", "female", "others"].includes(value)) {
+                throw new Error("Gender is Invalid !!!");
+            }
+        }
+    },
+    photoUrl : {
+        type : String,
+        default : "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_640.png"
+    },
+    about : {
+        type : String,
+    },
+    skills : {
+        type : [String],
+        validate(value){
+            if(value.length > 10) throw new Error("Maximum 10 Skills are allowed")
+        }
+    }
+},
+{
+    timestamps : true,
 });
 
 module.exports = mongoose.model('User', userSchema);
